94b4389ac394906570e349f1f2e79ae7676da30c
Austin Rowland <austin.rowland@postman.com>
2026-03-23 22:04:04 -0700

    fix: converted the script back to a standalone file, removed pom.xml file so it can be drag-and-dropped into other projects

--- dev-portal/java/script/PostmanService.java
+++ dev-portal/java/script/PostmanService.java
@@ -1,3 +1,5 @@
+package com.postman.sdk.script;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
