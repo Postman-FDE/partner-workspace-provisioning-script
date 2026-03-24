211ca6d5a29f3cef4b19ec4fa3051b436803570b
Austin Rowland <austin.rowland@postman.com>
2026-03-23 22:01:41 -0700

    fix: added a pom.xml file to the java script directory

--- dev-portal/java/script/PostmanService.java
+++ dev-portal/java/script/PostmanService.java
@@ -1,5 +1,3 @@
-package com.postman.sdk.script;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
